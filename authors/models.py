85de241ce501f24d29b420c19dd09214fd4dc724
Michael <kaitmen17@gmail.com>
2023-03-30 12:29:30 +0300

    Initial commit

---
+++ authors/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.conf import settings
+from django.db.models import Sum
+
+from . import TYPE_CHOICES
+
+
+class Author(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        # my_posts = self.posts
+        # post_comments = [p.post_comments.get().aggregate(sum=Sum('rating'))['sum'] for p in my_posts]
+        # post_comments = sum(post_comments)
+        sum_posts = sum(self.posts.values('rating')) * 3
+        sum_comments = sum(self.comments.values('rating'))
+        self.rating = sum_posts + sum_comments
+
+
+class Category(models.Model):
+    name = models.CharField(unique=True, max_length=256)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        Author, related_name='posts',
+        on_delete=models.CASCADE,
+    )
+    categories = models.ManyToManyField(Category)
+    type = models.CharField(max_length=1, choices=TYPE_CHOICES)
+    title = models.CharField(unique=True, max_length=256)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+
+    def preview(self):
+        return self.text[:124] + '...' if len(self.text) > 124 else self.text
+
+
+
+    def like(self):
+        self.rating += 1
+
+    def dislike(self):
+        self.rating -= 1
+
+
+
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='post_comments' )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='comments',
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    rating = models.IntegerField(default=0)
+
+
+    def like(self):
+        self.rating += 1
+
+    def dislike(self):
+        self.rating -= 1
